c6266531a689186fa729e868b8ce15c4442c3f92
iiluvmemo1 <edisoncruz258@gmail.com>
2025-11-24 10:46:49 +0800

    Update Portfolio

--- contact.js
+++ contact.js
@@ -1,13 +1,28 @@
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 
+// function toggleSideBar(){
+//     sidebar.classList.toggle('close')
+
+//     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+//         ul.classList.remove('show')
+//         ul.previousElementSibling.classList.remove('rotate')
+//     })
+// }
+
+
+// function toggleSubMenu(button){
+//     button.nextElementSibling.classList.toggle('show')
+//     button.classList.toggle('rotate')
+
+//     if(sidebar.classList.contains('close')){
+//         sidebar.classList.toggle('close')
+//         toggleButton.classList.toggle('rotate')
+//     }
+// }
+
 function toggleSideBar(){
     sidebar.classList.toggle('close')
-
-    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-        ul.classList.remove('show')
-        ul.previousElementSibling.classList.remove('rotate')
-    })
 }
 
 function toggleSubMenu(button){
@@ -18,8 +33,8 @@ function toggleSubMenu(button){
         sidebar.classList.toggle('close')
         toggleButton.classList.toggle('rotate')
     }
-    
 }
+
 
 function autoCloseSidebar() {
     if (window.innerWidth <= 800) {
